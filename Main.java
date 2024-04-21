26e771e1dc99807114e9581d11b1960b96f81982
shrutikhadake <148204864+shrutikhadake@users.noreply.github.com>
2024-04-21 20:06:53 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static final Scanner keyboard;
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Generator generator = new Generator(keyboard);
+        generator.mainLoop();
+        keyboard.close();
+    }
+
+    static {
+        keyboard = new Scanner(System.in);
+    }
+}
